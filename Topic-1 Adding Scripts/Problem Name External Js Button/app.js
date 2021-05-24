5ea287a96fdf0f0c99d51b3d9545531146d7f051
AbidaAli100 <abida.ali100@gmail.com>
2021-05-25 02:02:38 +0500

    linked html and JS file to HTML

---
+++ Topic-1 Adding Scripts/Problem Name External Js Button/app.js
@@ -0,0 +1,16 @@
+// to pop-up alert for "red" Button// 
+
+function Red(){
+   alert ('This is the alert for RED button.')
+}
+
+//alert for "blue" Button //
+
+function Blue(){
+    alert('This is the alert for Blue button.')
+}
+
+//alert for "green" Button//
+function Green(){
+    alert('This is the alert for Green button.')
+}
