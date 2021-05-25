22d30ef1c8a765f5c063f3bff7a47ef179fb2b7a
AbidaAli100 <abida.ali100@gmail.com>
2021-05-25 11:42:02 +0500

    added comments before the functions

--- Topic-1 Adding Scripts/Problem Name External Js Button/app.js
+++ Topic-1 Adding Scripts/Problem Name External Js Button/app.js
@@ -1,16 +1,16 @@
-// to pop-up alert for "red" Button// 
+// to pop-up alert clicking "red" Button// 
 
 function Red(){
    alert ('This is the alert for RED button.')
 }
 
-//alert for "blue" Button //
+//alert for clicking "blue" Button //
 
 function Blue(){
     alert('This is the alert for Blue button.')
 }
 
-//alert for "green" Button//
+//alert for clicking "green" Button//
 function Green(){
     alert('This is the alert for Green button.')
 }
