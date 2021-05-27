266ee90b2360e6da8fd1060de29a53a72afd3627
AbidaAli100 <abida.ali100@gmail.com>
2021-05-27 22:04:17 +0500

    added Js add Variable

---
+++ Topic-2  JS Variables/3- Add Variables/app.js
@@ -0,0 +1,11 @@
+/*Take two numbers 4 and 12. 
+Initialize them using "let" */
+
+let Numb1 = 4;
+let Numb2 = 12;
+
+//Add both of them and Save them in a new variable
+let Sum = (Numb1 + Numb2);
+
+// print it using console.log 
+console.log(Sum);
