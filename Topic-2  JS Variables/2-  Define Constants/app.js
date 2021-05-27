7db7d664eaa21955a2569b579c222af528d006c5
AbidaAli100 <abida.ali100@gmail.com>
2021-05-27 21:49:34 +0500

    finishes Js constants

---
+++ Topic-2  JS Variables/2-  Define Constants/app.js
@@ -0,0 +1,23 @@
+//Create a numeric, string, and boolean constants and print them
+
+//numeric Constant
+const Numb = 2000;
+console.log(Numb);
+
+
+
+//string Constant
+const string = (" JavaScript Variable");
+console.log(string);
+ 
+
+
+//boolean variable//
+
+//For true
+const isworking = true;
+console.log(isworking);
+
+//For False
+let isStudying = false;
+console.log(isStudying);
