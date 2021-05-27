945bb02540972625f040ab06192eff524dfcec6e
AbidaAli100 <abida.ali100@gmail.com>
2021-05-27 21:41:42 +0500

    added variables in JS

---
+++ Topic-2  JS Variables/1- Define Variables/app.js
@@ -0,0 +1,23 @@
+//Create a numeric, string, and boolean variable and print them//
+
+//numeric variable//
+var Number = 10;
+console.log(Number);
+
+
+
+//string variable//
+var String1 = (" JavaScript Code challange");
+console.log(String1);
+ 
+
+
+//boolean variable//
+
+//For true
+var isCoding = true;
+console.log(isCoding);
+
+//For False
+let isReading = false;
+console.log(isReading);
