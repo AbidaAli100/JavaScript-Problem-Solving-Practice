bbae3615370c03a0a070b4e9cdf97ceca591be1f
AbidaAli100 <abida.ali100@gmail.com>
2021-05-28 00:13:52 +0500

    finished JS comparsion operators

---
+++ Topic-3 Operators/1- Js Arithematic Operators/app.js
@@ -0,0 +1,34 @@
+// output of following:
+
+
+let x = 5;
+let y = 10;
+let z = 15;
+
+//x + y  -- to add
+let Sum = (x + y);
+console.log(Sum); //answer will be print on console.log
+
+// y - z -- for difference 
+let minus = (y - z);
+console.log(minus);
+
+//x * y  -- to multiply
+let multiply = (x * y);
+console.log(multiply);
+
+//y / x-- to divide
+let divide = (y / x);
+console.log(divide);
+
+//x % 2 -- for reminder
+let Reminder = (x % 2);
+console.log(Reminder);
+
+// x ++  -- for increment
+let inctr = (x++);
+console.log(inctr);
+
+//x--   -- for decrement
+let decre = (x--);
+console.log(decre);
