e40c4af1f6cc8e02bc9b5517720ec7fdb65a62e5
AbidaAli100 <abida.ali100@gmail.com>
2021-05-28 19:20:22 +0500

    converted celcius in fehrenheit

---
+++ Topic-3 Operators/4- Problem 4/app.js
@@ -0,0 +1,15 @@
+/* Write a program that converts degree to fahrenheit and fahrenheit to degrees.
+
+ F = ( 1.8 * C) + 32
+
+ Where,
+ 
+ F = Temperature in Fahrenheit
+ C = Temperature in degree Celsius
+ We can simply convert temperature using the above formula.*/
+
+ // will pop-up a prompt, where user will write the the degree celcius.
+let C = prompt ("Temprature Celcius is");
+let f = ( 1.8 * C) + 32;      
+console.log(f); // will print the value in console log
+
