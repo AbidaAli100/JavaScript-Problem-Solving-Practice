68bb242e6a9df6c97958c744b047d8f1839b3625
AbidaAli100 <abida.ali100@gmail.com>
2021-06-01 16:48:22 +0500

    checked the data types

---
+++ Topic-6 Datatypes/1- checkIt/app.js
@@ -0,0 +1,25 @@
+
+//checking data type of variables
+
+let a = 42; // integer value
+console.log(a); // typeof 42 = "number"
+
+let b = 20.7; // decimal value
+console.log(b); // typeof 20.7 = "number"
+
+let c = false; // boolean value 
+console.log(c); //typeof false id "boolean"
+
+let d ="TechKaro"; // checking string without quotation marks 
+console.log(d); //typeof "d" is "string"
+
+// let e = TechKaro ; 
+// console.log(e);
+/*removing quotation marks from  the string gives an error:
+"Cannot access 'e' "..... because the value without quotation marks
+is not a string */
+
+let f = null; // assign null value
+console.log(f); // typeof null is "null"
+
+
