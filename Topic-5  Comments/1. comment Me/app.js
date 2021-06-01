1f50134b19e8c34a5e2588fe4deb8c150010ccce
AbidaAli100 <abida.ali100@gmail.com>
2021-06-01 15:56:52 +0500

    added multi-lines comments in Js

---
+++ Topic-5  Comments/1. comment Me/app.js
@@ -0,0 +1,11 @@
+// For adding more than one line comment in JS , we use "/* text... next line */" e.g.
+
+/* step 1 = declare two different numbers variables 
+   step 2 = declare another variable, as a sum of the first two variable
+   step 3 = print the sum in the console.log/ alert */
+
+   let numb1 = 100;
+   let numb2 = 50;
+   
+   let sum = (numb1+numb2);
+   alert(sum);
