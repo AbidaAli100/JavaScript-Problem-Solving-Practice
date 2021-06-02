758077b4e267593cdc915b9d2e2da2a91ac37c15
AbidaAli100 <abida.ali100@gmail.com>
2021-06-02 21:19:34 +0500

    finished checkVariable

---
+++ Topic-7  Variables/03- Problem- Check Variable/app.js
@@ -0,0 +1,13 @@
+
+/*
+    Create a function called myFunc and declare
+     a variable name inside the body of that function. 
+    In the console check the value of the name and see what console prints.
+*/
+
+function myFunc() {
+    let name = "abida";
+    console.log(name);  /* the "valueOf name" prints 
+                        "Uncaught SyntaxError: Unexpected identifier"*/
+}
+
