eec6be3cc9b47d6e471e929878cf3467e925aca9
AbidaAli100 <abida.ali100@gmail.com>
2021-06-03 21:19:20 +0500

    finished asignment operators

---
+++ Topic-9  Operators/2- Assignment Operators/app.js
@@ -0,0 +1,10 @@
+
+// What will happen if;   x=3; x+1; x+=3;
+
+let x = 3; // the value of x will be three
+
+
+x + 1; //  the answer will be 4. (increase 1)
+
+x += 3;  // the answer will be 6, (double)
+
