1aeb6f11a58f58932b35311d7f9b21524ae5d7a2
AbidaAli100 <abida.ali100@gmail.com>
2021-06-03 21:30:14 +0500

    finished logical operators

---
+++ Topic-9  Operators/3- Logical Operators/app.js
@@ -0,0 +1,23 @@
+
+
+/*What happens if we write:
+
+
+!(!(4>2)) */
+
+true && true; // both are true, so the aswer will be true
+
+false && false; // both are false, so the answer will be true
+
+false || true; // one among them is true, so the answer will be true
+
+5 > 4 && 2 < 5; // both are true, so the answer will be true
+
+2 == 9 || 2==2; // one statement is true, the answer will be true
+
+!(!(4>2)); //  !4>2 is false but !(!(4>2)) is true
+
+
+
+
+
