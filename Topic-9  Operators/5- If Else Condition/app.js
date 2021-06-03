1ad9c51caf4879ce9bd176ded84cfebe6c08729f
AbidaAli100 <abida.ali100@gmail.com>
2021-06-03 21:50:39 +0500

    finished if else condition

---
+++ Topic-9  Operators/5- If Else Condition/app.js
@@ -0,0 +1,22 @@
+/*
+    Write the if else statement by using the condition
+     that if student has gained marks more than or
+     equal to 50 then student is pass else student is fail.
+*/
+
+let passingMarks = 50;
+let obtMarks = 60;
+
+if (obtMarks > passingMarks) {
+    alert(' 1st Student  is Pass')
+} else {
+    alert(" 1st student is Fail");
+}
+
+let obtMarks2 = 33;
+if (obtMarks2 > passingMarks) {
+    alert(' 2nd Student is Pass')
+} else {
+    alert(" 2nd student is Fail");
+}
+
