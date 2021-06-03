1359209ce72a7e52a6b46b50b9c706edee349129
AbidaAli100 <abida.ali100@gmail.com>
2021-06-03 21:10:47 +0500

    finished comparsion opeators

---
+++ Topic-9  Operators/1- ComparisonOperators/app.js
@@ -0,0 +1,72 @@
+
+// List down the comparison operators.
+
+//comparsion operators will always give the answer in boolean type
+
+// The comparsion operators are below, with examples
+
+//equal to
+let a = 10;
+let b = 10;
+let c = (a == b);
+console.log(c); // true
+
+
+//equal value and equal type 
+let d = "abida";
+let e = "abida";
+let f = (d === e);
+console.log(f);  // true
+
+
+//not equal t0
+let g = 66;
+let h = 88;
+let i = (g != h);
+console.log(i); //true
+
+
+//not equal type and value
+let j = 12;
+let k = "12";
+let l = (j !== k);
+console.log(l); // true
+
+
+//greater than
+let m = 33;
+let n = 66;
+let o = (n > m); //n is greater than m
+console.log(o);  // true
+
+
+//less than
+let p = 33;
+let q = 66;
+let r = (p < q); //p is less than q
+console.log(r);  // true
+
+
+//greater than or equal to
+
+let s = 44;
+let t = 44;
+let u = (s >= t);
+console.log(u);
+
+
+// less than or equal to
+let v = 45;
+let w = 77;
+let x = (v <= w);
+console.log(x);  // true
+
+
+// 2-- What happens if you write " 4<=5?"
+
+4 <= 5;     // less than or is equal to
+// the answer will be true because  4 is les than 5
+
+
+
+
