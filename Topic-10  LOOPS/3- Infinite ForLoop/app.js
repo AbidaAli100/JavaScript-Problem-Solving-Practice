e90d1990aee6f097fd6932ea9e0cd3e1718f4a2d
AbidaAli100 <abida.ali100@gmail.com>
2021-06-04 19:58:41 +0500

    finishes infinite loop

---
+++ Topic-10  LOOPS/3- Infinite ForLoop/app.js
@@ -0,0 +1,11 @@
+
+/* Create a for loop.
+
+  Use the given condition and see what happens. 
+  for (;;) {}
+*/
+
+for (let i = 200; i >= 0; i -1) {
+    console.log(i);
+}
+// this an infinit for loop.. it never stops
