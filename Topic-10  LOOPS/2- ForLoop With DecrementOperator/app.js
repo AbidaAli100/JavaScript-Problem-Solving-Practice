720367abb955f35d3887d36466704930c19790b8
AbidaAli100 <abida.ali100@gmail.com>
2021-06-04 16:08:40 +0500

    finished decrement for loop

---
+++ Topic-10  LOOPS/2- ForLoop With DecrementOperator/app.js
@@ -0,0 +1,13 @@
+/*
+        Create a for loop.
+
+     1-Take the loop iterating variable as i.
+     2-Initialize it with 200
+     3-Print numbers from 200 till 0.
+     4-Use the decrement operator as -=3.
+*/
+
+for (let i = 200; i >= 0; i -= 3) {
+    console.log(i);
+}
+// the decrement stops at 2, because the increment operator is -3.
