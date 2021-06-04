a19a6ba41599a504ed077c9b3e821c3afbdfc36c
AbidaAli100 <abida.ali100@gmail.com>
2021-06-04 20:29:25 +0500

    finished do-while loop

---
+++ Topic-10  LOOPS/5- Do-WhileLoop/app.js
@@ -0,0 +1,13 @@
+
+/* 
+  1- Take a loop control variable as a.
+  2-Initialize it.
+  3-Using While loop, print your name 10 times on the console.
+*/
+
+var a= 0;
+do{
+  console.log("ABIDA");
+  a++;
+}
+ while (a < 10);
