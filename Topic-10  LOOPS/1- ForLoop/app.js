fbcf343b3dbf9bfd1e3c856c41149f006672b26a
AbidaAli100 <abida.ali100@gmail.com>
2021-06-04 15:32:56 +0500

    finished for loop

---
+++ Topic-10  LOOPS/1- ForLoop/app.js
@@ -0,0 +1,11 @@
+/*   
+          --Create a Loop--
+    1- Take a loop iterating variable as i.
+    2- The loop should iterate less than 50 times.
+    3- You have to print “Clap!” on the console.
+*/
+
+
+for (let i = 0; i < 50; i++){
+    console.log("CLAP!");
+}
