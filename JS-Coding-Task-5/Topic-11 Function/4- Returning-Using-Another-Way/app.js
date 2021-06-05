9cc10f3b2741854ae3e3b16e9af8f70edb595932
AbidaAli100 <abida.ali100@gmail.com>
2021-06-05 22:12:07 +0500

    finished coding task 5

---
+++ JS-Coding-Task-5/Topic-11 Function/4- Returning-Using-Another-Way/app.js
@@ -0,0 +1,11 @@
+/*
+    Using the above example,
+     return the answer without storing it in any variable.
+*/
+
+function AddTwoNumbers(num1, num2){
+    let answer = (num1 + num2);
+    console.log(num1 + num2);
+    return num1 + num2;
+    }
+    AddTwoNumbers(12,4); 
