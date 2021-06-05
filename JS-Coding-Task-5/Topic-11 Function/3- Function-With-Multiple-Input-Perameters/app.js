3172178da34a6152b7fbefa9cc5c31747fbfa961
AbidaAli100 <abida.ali100@gmail.com>
2021-06-05 21:44:16 +0500

    finished function with different peramiters

---
+++ JS-Coding-Task-5/Topic-11 Function/3- Function-With-Multiple-Input-Perameters/app.js
@@ -0,0 +1,14 @@
+/*
+    1- Create a function with the name “AddTwoNumbers.”
+    2- Take two input parameters using names “Num1” and “Num2.”
+    3- Perform addition operation on these input numbers.
+    4- Save the result in variable Sum.
+    5- Return the result.
+*/
+
+function AddTwoNumbers(num1, num2){
+let answer = (num1 + num2);
+console.log(answer);
+return answer;
+}
+AddTwoNumbers(12,4);
