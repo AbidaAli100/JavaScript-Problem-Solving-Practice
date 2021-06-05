1d8d94f58476b1e9ccc5475d3a76e9b4ead755fc
AbidaAli100 <abida.ali100@gmail.com>
2021-06-05 21:24:15 +0500

    finished intro to function

---
+++ JS-Coding-Task-5/Topic-11 Function/1- Intro-To-Function/app.js
@@ -0,0 +1,14 @@
+/*
+   1-  Write a function with the name Double.
+    2-Create a variable answer as numeric data type.
+    3-Function takes paisa as input parameter.
+    4-Multiply paisa with 2 and store the result in answer.
+      Return answer. 
+*/
+var numb = 12;
+function Double(numb) {
+
+  let answer = numb * 2;
+  return answer;
+}
+console.log(Double);
