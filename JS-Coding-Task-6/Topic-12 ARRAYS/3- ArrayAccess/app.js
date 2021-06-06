0d72887776abf063f59584ddb0154e58f6bee973
AbidaAli100 <abida.ali100@gmail.com>
2021-06-06 22:19:00 +0500

    finished array access

---
+++ JS-Coding-Task-6/Topic-12 ARRAYS/3- ArrayAccess/app.js
@@ -0,0 +1,6 @@
+
+//Print the first and last member of the same array
+
+let games = ["football", "volleyball", "cricket", "MMA", "basketball"]
+console.log("First element" + ":" + games[0]);
+console.log("Last element" + ":" + games[4]);
