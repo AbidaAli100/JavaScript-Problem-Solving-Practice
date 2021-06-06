f0fd89849ba70cb0bedb2ff35e01827d1ddbac7b
AbidaAli100 <abida.ali100@gmail.com>
2021-06-06 22:24:52 +0500

    finshed array length

---
+++ JS-Coding-Task-6/Topic-12 ARRAYS/4- Array Length/app.js
@@ -0,0 +1,5 @@
+
+//Print the first and last member of the same array
+
+let games = ["football", "volleyball", "cricket", "MMA", "basketball"]
+console.log("length of the Array" + ": " + games.length); //length of the Array
