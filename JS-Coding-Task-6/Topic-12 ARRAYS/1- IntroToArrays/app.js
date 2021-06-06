8795458fea6034b936dc3f8d19d265b4839f1d08
AbidaAli100 <abida.ali100@gmail.com>
2021-06-06 21:54:19 +0500

    finshed intro to array

---
+++ JS-Coding-Task-6/Topic-12 ARRAYS/1- IntroToArrays/app.js
@@ -0,0 +1,11 @@
+/*
+    Initialize an array by the name of games. Add any of the five games to it.
+     Print this array member one by one by indexing (not using for loop).
+*/
+
+let games = ["football", "volleyball", "cricket", "MMA", "basketball"]
+console.log(games[0]);
+console.log(games[1]);
+console.log(games[2]);
+console.log(games[3]);
+console.log(games[4]);
