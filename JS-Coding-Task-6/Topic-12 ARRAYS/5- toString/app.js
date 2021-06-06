9eb950cc131cdbf1cb429fed842c3ed64e4cf72d
AbidaAli100 <abida.ali100@gmail.com>
2021-06-06 22:38:46 +0500

    finished toString in Array

---
+++ JS-Coding-Task-6/Topic-12 ARRAYS/5- toString/app.js
@@ -0,0 +1,5 @@
+
+//Print the first and last member of the same array
+
+let games = ["football", "volleyball", "cricket", "MMA", "basketball"]
+console.log(games.toString()); //it was alrady in string
