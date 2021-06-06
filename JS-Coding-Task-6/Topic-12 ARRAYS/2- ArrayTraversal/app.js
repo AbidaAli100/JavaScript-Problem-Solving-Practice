8bd12f3424d835846f773a5ef23e7b218829ba76
AbidaAli100 <abida.ali100@gmail.com>
2021-06-06 22:11:25 +0500

    finished Array Traverse

---
+++ JS-Coding-Task-6/Topic-12 ARRAYS/2- ArrayTraversal/app.js
@@ -0,0 +1,10 @@
+/*
+    Traverse the array using a for loop
+Print its members and index using the same for loop 
+*/
+
+let games = ["football", "volleyball", "cricket", "MMA", "basketball"]
+ 
+for (let i = 0; i<games.length; i++){ //using for loop
+     console.log(i + " " + games[i] );
+}
