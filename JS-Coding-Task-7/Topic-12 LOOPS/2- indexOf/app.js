a77360cf2d062f3d81310417e01248683a7b59dc
AbidaAli100 <abida.ali100@gmail.com>
2021-06-07 21:31:34 +0500

    Finished forIn Loop

---
+++ JS-Coding-Task-7/Topic-12 LOOPS/2- indexOf/app.js
@@ -0,0 +1,9 @@
+/*
+    Use indexOf function to attempt the same problem 
+    and print index of ‘Cricket’ member
+*/
+
+
+let games = ["Football", "Tennis", "MMA", "Cricket", "Volleyball"]
+let a = games.indexOf("Cricket");
+console.log(a);  // the answer is 3
