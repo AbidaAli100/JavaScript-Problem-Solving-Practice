483135f3866ab735a7f9d91c963d52b4fe4ac6fa
AbidaAli100 <abida.ali100@gmail.com>
2021-06-07 21:41:09 +0500

    Finished LastIndexOf

---
+++ JS-Coding-Task-7/Topic-12 LOOPS/3- lastIndexOf/app.js
@@ -0,0 +1,9 @@
+/*
+    Use the lastIndexOf function to find a ‘Cricket’ member.
+     Start from the 3rd index and print its index.
+*/
+
+
+let games = ["Football", "Tennis", "MMA", "Cricket", "Volleyball"]
+let a = games.lastIndexOf("Cricket");
+console.log(a);  //the answer is "3"
