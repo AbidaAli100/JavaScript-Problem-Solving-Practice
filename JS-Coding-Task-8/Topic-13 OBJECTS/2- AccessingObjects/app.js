626f44f30e4cc786f5ac56de93b22c22718f5eb8
AbidaAli100 <abida.ali100@gmail.com>
2021-06-08 22:17:03 +0500

    finished accessing object

---
+++ JS-Coding-Task-8/Topic-13 OBJECTS/2- AccessingObjects/app.js
@@ -0,0 +1,17 @@
+
+/*
+   1- Make an object. Name it as cities. 
+   2-Now give it keys as ‘first’,’ second’,’ third’,’ fourth’.
+   Put ‘Karachi’, ‘Lahore’, ‘Islamabad’, ‘Quetta’ as keys.
+   3- Access the first and fourth key of the object and print their respective values
+*/
+
+var cities = {
+    first: "Karachi",
+    second: "Lahore",
+    third: "Islamabad",
+    fourth: "Quetta"
+}
+console.log(cities.first);
+console.log(cities.fourth);
+
