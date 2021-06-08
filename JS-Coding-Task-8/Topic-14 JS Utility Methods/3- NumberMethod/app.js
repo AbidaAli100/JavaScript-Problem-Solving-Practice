613f5f38cd2fa76461bd17ec54850e397372b459
AbidaAli100 <abida.ali100@gmail.com>
2021-06-08 23:03:29 +0500

    finished number method

---
+++ JS-Coding-Task-8/Topic-14 JS Utility Methods/3- NumberMethod/app.js
@@ -0,0 +1,11 @@
+/*
+  Use the Number method to convert the given 
+  variable age = ‘44’ and find it’s type before and after the conversion
+*/
+
+var age = "44";
+console.log(typeof age); // string
+
+let ageNumb = parseInt("44");
+console.log(typeof ageNumb); //number
+
