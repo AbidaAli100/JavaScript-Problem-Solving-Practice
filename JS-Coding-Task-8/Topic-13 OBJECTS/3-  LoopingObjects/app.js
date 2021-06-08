2f064b0c5afccb7347f549e470d85684d8a4b361
AbidaAli100 <abida.ali100@gmail.com>
2021-06-08 22:22:24 +0500

    finished looping values

---
+++ JS-Coding-Task-8/Topic-13 OBJECTS/3-  LoopingObjects/app.js
@@ -0,0 +1,16 @@
+
+// Loop values of object’s properties by using for...in loop and print them.
+
+var cities = {
+    first: "Karachi",
+    second: "Lahore",
+    third: "Islamabad",
+    fourth: "Quetta"
+}
+var i = 0;
+for (i in cities) {
+    console.log(cities[i]);
+}
+
+
+
