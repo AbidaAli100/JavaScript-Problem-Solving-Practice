9c5a0caf6c25c1f94da2fce0b1a1b899247f7d1a
AbidaAli100 <abida.ali100@gmail.com>
2021-06-08 17:49:40 +0500

    finished objects

---
+++ JS-Coding-Task-8/Topic-13 OBJECTS/1- Objects/app.js
@@ -0,0 +1,14 @@
+
+/*
+   1- Make an object. Name it as cities. 
+   2-Now give it keys as ‘first’,’ second’,’ third’,’ fourth’.
+   Put ‘Karachi’, ‘Lahore’, ‘Islamabad’, ‘Quetta’ as keys.
+*/
+
+var cities = {
+    first: "Karachi",
+    second: "Lahore",
+    third: "Islamabad",
+    fourth: "Quetta"
+}
+console.log(cities);
