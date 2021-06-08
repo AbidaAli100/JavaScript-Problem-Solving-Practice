34116a200f106ef35c0e652b1179fa0f7be6bf11
AbidaAli100 <abida.ali100@gmail.com>
2021-06-08 23:30:16 +0500

    finished date method

--- JS-Coding-Task-8/Topic-14 JS Utility Methods/5- DateMethod/app.js
+++ JS-Coding-Task-8/Topic-14 JS Utility Methods/5- DateMethod/app.js
@@ -1,14 +1,6 @@
 /*
-   A-- Suppose you’re given this array: let agesArray = [12,45,56,4].
-    Use map method to print all of the values and their indices.
-    After that use map method to make a new array from the same array
-     by multiplying each age value by 10.
-   B -- Suppose you’re given an array of objects
+  Use Date() method to get the current data and print it on the console
 */
 
-let agesArray = [12, 45, 56, 4]
-var a = agesArray.map(mapFunction);
-function mapFunction(numb) {
-   return numb * 10;
-}
+var a = new Date();
 console.log(a);
