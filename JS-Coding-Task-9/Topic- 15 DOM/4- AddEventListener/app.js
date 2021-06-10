dce034f798f912b88e5a479e6389e14d8b0f86af
AbidaAli100 <abida.ali100@gmail.com>
2021-06-10 22:38:49 +0500

    finished eventselector

--- JS-Coding-Task-9/Topic- 15 DOM/4- AddEventListener/app.js
+++ JS-Coding-Task-9/Topic- 15 DOM/4- AddEventListener/app.js
@@ -1,9 +1,5 @@
 
-
-// Make a button in your html file with some text.
-// Now through addEvenListener, add a click event to it.
-// Call a function on this event.
-// In that function, alert some text
+// alert on the function
 
 function addEvenListener(){
     alert("HEY, YOU CLICKED THE BUTTON");
