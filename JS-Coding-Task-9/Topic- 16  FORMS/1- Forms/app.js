c136b4480ce1f754fd242b447d92d21ab1d9c2ac
AbidaAli100 <abida.ali100@gmail.com>
2021-06-11 01:15:40 +0500

    modified task 9

--- JS-Coding-Task-9/Topic- 16  FORMS/1- Forms/app.js
+++ JS-Coding-Task-9/Topic- 16  FORMS/1- Forms/app.js
@@ -1,22 +1,24 @@
 
 
 
-// Now write a function that’d be called on that event
-// Check that whether values of both email and password
-//  fields are empty or not using the same function
-//     If any of the fields is empty, ask use to fill up the fields
-// If both are filled, show an alert saying ‘Logged In Successfully’.
 // Also, check for format of email in the first input field and ensure
 //  that password field’s length is not less than 8 character.
 
+document.querySelector("#button").onclick = addEventListener;
 function addEventListener() {
-    let emailValue = document.getElementById(email).value;
+    const element1 = document.getElementById("email");
+    const element2 = document.getElementById("password");
 
-    let passwordValue = document.getElementById(password).value;
+    if (element1.value == "" && element2.value == "") {
+        alert("Fill the requirments first");
+    }
+    else if(element1.value == "" || element2.value == ""){
+        alert("fill all the blanks");
+    }
+    else {
+        alert("logged in successfully");
+    } 
 
-    if (emailValue == "" || passwordValue == "") {
-        alert("Fill the requirments first")
-    } else {
-        alert("Logged In Successfully")
-    } console.log(emailValue);
 }
+
+
